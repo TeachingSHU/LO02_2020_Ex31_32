f168ae2dd6cef2f7752312b797144e83d5ba08d9
Lunde CHEN <lunde.chen@yahoo.fr>
2020-11-01 09:33:34 +0800

    commit

---
+++ cmake-build-debug/CMakeFiles/LO02_2020_Ex31.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LO02_2020_Ex31.dir/evenement.cpp.obj"
+  "CMakeFiles/LO02_2020_Ex31.dir/main.cpp.obj"
+  "LO02_2020_Ex31.exe"
+  "LO02_2020_Ex31.exe.manifest"
+  "LO02_2020_Ex31.pdb"
+  "libLO02_2020_Ex31.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LO02_2020_Ex31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ evenement.cpp
@@ -0,0 +1,47 @@
+//
+// Created by lunde on 2020/11/1.
+//
+
+#include "evenement.h"
+
+using namespace TIME;
+using namespace std;
+
+ostream& operator<<(ostream& f, const Evt& e){
+    e.afficher(f);
+    return f;
+}
+
+void Agenda::afficher(std::ostream& f) const {
+    /*
+    for(unsigned int i = 0 ; i < events.size() ; i++) {
+        events[i]->afficher(f);
+    } */
+
+
+    for(vector<Evt *>::const_iterator it = events.begin() ;
+        it != events.end() ;
+        it++) {
+        (*it)->afficher(f);
+    }
+
+}
+
+Evt1j* Evt1j::clone() const { return new Evt1j(*this); }
+Evt1jDur* Evt1jDur::clone() const { return new Evt1jDur(*this); }
+Rdv* Rdv::clone() const { return new Rdv(*this); }
+EvtPj* EvtPj::clone() const { return new EvtPj(*this); }
+
+//Agenda& Agenda::operator<<(Evt& e) {
+//    events.push_back(&e);
+//    return *this;
+//}
+
+Agenda& Agenda::operator<<(Evt& e) {
+    events.push_back(e.clone());
+    return *this;
+}
+
+Agenda::~Agenda() {
+    for (unsigned int i = 0; i < events.size(); i++) delete events[i];
+}
